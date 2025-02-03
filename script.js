80f86fb4db13688f9688b1b66fb6a2fb7a40c2e0
Moses  Oyibo Orha <moze4u2002@gmail.com>
2025-02-03 13:28:50 +0100

    Added scripts

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// Get elements
+const homeSection = document.getElementById('home');
+const discoverSection = document.getElementById('discover');
+const createSection = document.getElementById('create');
+const communitySection = document.getElementById('community');
+
+// Add event listeners
+homeSection.addEventListener('click', () => {
+    console.log('Home section clicked');
+});
+
+discoverSection.addEventListener('click', () => {
+    console.log('Discover section clicked');
+});
+
+createSection.addEventListener('click', () => {
+    console.log('Create section clicked');
+});
+
+communitySection.addEventListener('click', () => {
+    console.log('Community section clicked');
+});
+
+
